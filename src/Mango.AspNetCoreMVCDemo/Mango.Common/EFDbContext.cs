ff9b7113dd3a7adee4f21acd8cd6671f846f2d76
51core <service@51core.net>
2019-07-12 16:25:58 +0800

    20190712-创建公共类库项目

---
+++ src/Mango.AspNetCoreMVCDemo/Mango.Common/EFDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mango.Common
+{
+    public class EFDbContext
+    {
+
+    }
+}
---
+++ src/Mango.AspNetCoreMVCDemo/Mango.Common/Entity/51core_User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Common.Entity
+{
+    public class _51core_User
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        [Key]
+        public int? UserId { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int? Age { get; set; }
+    }
+}
